181346d8253f1eb0bdd93e1fcf8683cc397263f4
davinash97 <avinashd2904@gmail.com>
2025-08-28 22:56:57 +0530

    Update Contact: - Add Contact Card

---
+++ src/app/components/ContactCard.tsx
@@ -0,0 +1,26 @@
+type ContactProps = {
+	title: string;
+	image: string;
+	content: string;
+};
+
+import Image from "next/image";
+
+export default function ContactCard({ title, image, content }: ContactProps) {
+	return (
+		<div className="flex flex-row w-fit justify-between gap-2 rounded-[20] p-3 border-2 border-solid border-red-200">
+			<div className="flex flex-row relative h-[100] w-[100]">
+				<Image
+					src={image || "https://dummyimage.com/600x400/000/fff"}
+					alt={title}
+					fill={true}
+					loading="lazy"
+				/>
+			</div>
+			<div className="flex flex-col justify-center gap-3 text-left">
+				<h3 className="font-semibold">{title}</h3>
+				<div>{content}</div>
+			</div>
+		</div>
+	);
+}
--- src/app/page/contact/Contact.tsx
+++ src/app/page/contact/Contact.tsx
@@ -1,16 +1,34 @@
+import ContactCard from "@/app/components/ContactCard";
+
 export default function Contact() {
 	return (
 		<section className="flex flex-col w-screen text-center items-center p-10 gap-10">
 			{/* Contact */}
 			<h2 className="text-4xl">
-				<span>C</span>
-				<span>o</span>
+				<span>G</span>
+				<span>e</span>
+				<span>t </span>
+				<span>&nbsp;</span>
+				<span>i</span>
 				<span>n</span>
 				<span>t</span>
-				<span>a</span>
-				<span>c</span>
+				<span>o</span>
+				<span>&nbsp;</span>
 				<span>t</span>
+				<span>o</span>
+				<span>u</span>
+				<span>c</span>
+				<span>h</span>
 			</h2>
+			<div className="flex flex-col gap-3 w-[150]">
+				<ContactCard title="Email" image={""} content="this@mail.com" />
+				<ContactCard title="Phone" image={""} content="+91987654321" />
+				<ContactCard
+					title="Location"
+					image={""}
+					content="Bangalore, KA, India"
+				/>
+			</div>
 		</section>
 	);
 }
